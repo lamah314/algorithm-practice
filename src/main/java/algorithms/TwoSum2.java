d4a12ea258c9fdf292c4b33718860af4b16e2ea9
lamah314 <lamah314@gmail.com>
2019-02-06 22:19:11 -0500

    online solution of TwoSum

---
+++ src/main/java/algorithms/TwoSum2.java
@@ -0,0 +1,18 @@
+package algorithms;
+
+import java.util.*;
+
+public class TwoSum2 {
+	public int[] twoSum(int[] nums, int target) {
+	    Map<Integer, Integer> map = new HashMap<>();
+	    for (int i = 0; i < nums.length; i++) {
+	        int complement = target - nums[i];
+	       
+	        if (map.containsKey(complement)) { //check if hashmap has answer, check is instantaneous vs my iteration
+	            return new int[] { map.get(complement), i };
+	        }
+	        map.put(nums[i], i); //fill everything up to i in hashmap.
+	    }
+	    throw new IllegalArgumentException("No two sum solution");
+	}
+}
